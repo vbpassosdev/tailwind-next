4a1a8277ddecddd7260df783e5da71a496d14b02
vbpassosdev <vanessapassosdeveloper@gmail.com>
2025-07-07 14:53:02 -0300

    sidebar

--- src/app/components/Sidebar/MainNavigation/index.tsx
+++ src/app/components/Sidebar/MainNavigation/index.tsx
@@ -1,14 +1,18 @@
-import { Home } from "lucide-react";
+import { BarChart2, Folder, Home, LayoutDashboard, ListChecks, Users } from "lucide-react";
+import { NavItem } from "./NavItem";
 
 
 export function MainNavigation(){
     return(
         <nav className="space-y-0.5 ">
-            <a href="">
-            <Home className="h-5 w-5 text-zinc-500 "/>
-          
-            <span className="font-medium text-zinc-700">Home</span>
-            </a>
+
+          <NavItem title="Home" icon={Home}/>
+          <NavItem title="Dashboard" icon={LayoutDashboard}/>
+          <NavItem title="Projects" icon={Folder}/>
+          <NavItem title="Tasks" icon={ListChecks}/>
+          <NavItem title="Reporting" icon={BarChart2}/>
+          <NavItem title="Users" icon={Users}/>
+
         </nav>
     )
 }
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { Search } from "lucide-react";
 import { Logo } from "./logo";
+import { MainNavigation } from "./MainNavigation";
 
 export function Sidebar(){
     return <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
@@ -9,6 +10,8 @@ export function Sidebar(){
             <Search className="h-5 w-5 text-zinc-500" />  
             <input className="flex-1 border-0 gb-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search" />
             </div> 
+            
+            <MainNavigation></MainNavigation>
    
         </aside>
 }
